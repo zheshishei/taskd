5ecd57964c6c6504a07e96f32ae01a74a47f0b2c
Alan Sun <asun0927@gmail.com>
2020-04-04 12:58:09 -0700

    Updates

---
+++ precache-manifest.f1eb6459c29829760fc6d5f55fb35335.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2924b8cc9e6cfd0f97dc674a2ad25a",
+    "url": "/taskd/index.html"
+  },
+  {
+    "revision": "4f494017955c108796dd",
+    "url": "/taskd/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c18da0db207fb1068e1e",
+    "url": "/taskd/static/js/2.29fc4801.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/taskd/static/js/2.29fc4801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f494017955c108796dd",
+    "url": "/taskd/static/js/main.ae36d860.chunk.js"
+  },
+  {
+    "revision": "6f3a6567f47b3242af07",
+    "url": "/taskd/static/js/runtime-main.32703758.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taskd/static/media/logo.5d5d9eef.svg"
+  }
+]);
